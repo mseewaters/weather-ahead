237c5d7429e64a9ea5836cce363ca90b2e609077
Marjorie Waters <see_waters@comcast.net>
2024-05-14 22:42:09 -0400

    post api added

--- src/customize/userProfile.vue
+++ src/customize/userProfile.vue
@@ -26,7 +26,7 @@
   import axios from 'axios';
 
   const userProfile = ref({})
-  const error = ref('')
+  //const error = ref('')
   const settings = ref({
     enableNotifications: false,
     zipCode: ''
@@ -35,26 +35,62 @@
   // Define the JWT token and API URL
   const baseApiUrl = 'https://122jfj0o5h.execute-api.us-east-1.amazonaws.com/prod/user'
   const jwtToken = sessionStorage.getItem('token');
-  
 
-  
-  const fetchUserProfile = async (username) => {
-    try {
-      const response = await axios.get(`${baseApiUrl}?username=${username}`, {
-        headers: {
-          Authorization: `Bearer ${jwtToken}`
-        }
-      })
-      userProfile.value = response.data
-    } catch (err) {
-      error.value = 'Error fetching user profile: ' + err.message
-      console.error('Error fetching user profile:', err)
-    }
-  }
-  // Fetch the user profile when the component is mounted
-  onMounted(() => {
+  // const fetchUserProfile = async (username) => {
+  //   try {
+  //     const response = await axios.get(`${baseApiUrl}?username=${username}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${jwtToken}`
+  //       }
+  //     })
+  //     userProfile.value = response.data
+  //   } catch (err) {
+  //     error.value = 'Error fetching user profile: ' + err.message
+  //     console.error('Error fetching user profile:', err)
+  //   }
+  // }
+
+  const fetchUserProfile = async () => {
+  try {
     const username = sessionStorage.getItem('username');
-    fetchUserProfile(username)
+    
+
+    const response = await axios.get(`${baseApiUrl}?username=${username}`, {
+      headers: { Authorization: `Bearer ${jwtToken}` }
+    });
+
+    userProfile.value = response.data
+
+    if (response.data) {
+      settings.value.enableNotifications = response.data.enableNotifications || false;
+      settings.value.zipCode = response.data.zipCode || '';
+    }
+    } catch (error) {
+      console.error('Failed to fetch user profile:', error);
+      alert('Failed to load user settings.');
+    }
+  };
+
+  const submitForm = async () => {
+    try {
+      const username = sessionStorage.getItem('username');
+      const response = await axios.post(`${baseApiUrl}?username=${username}`, {
+        enableNotifications: settings.value.enableNotifications,
+        zipCode: settings.value.zipCode
+        }, 
+        {
+        headers: { Authorization: `Bearer ${jwtToken}` }
+        });
+      alert('Settings updated successfully!');
+      console.log(response)
+    } catch (error) {
+      console.error('Failed to update settings:', error);
+      alert('Failed to update settings.');
+    }
+  };
+  
+  onMounted(() => {
+    fetchUserProfile()
   })
   </script>
   
